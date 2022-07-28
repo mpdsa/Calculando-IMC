0b29a5a0d215b77b9b2ebd79fcb53d6e761c7981
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-28 13:52:39 -0300

    Initial commit

--- src/main/java/com/trybe/acc/java/Imc.java
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -3,24 +3,24 @@ package com.trybe.acc.java;
 import java.util.Locale;
 import java.util.Scanner;
 
+/** Calcula o Índice de Massa Corporal. */
 public class Imc {
 
-	public static void main(String[] args) {
-			// Ler a entrada de um número pelo console	
-			Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-			
-			System.out.println("Digite o peso:");
-			float peso = scan.nextFloat();
-			
-			System.out.println("Digite a aultura:");
-			float altura = scan.nextFloat();
-			
-			// Seu código aqui
-			
-					
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println(imc);
+  /** Imprime a saída no console. */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-	}
+    System.out.println("Digite o peso:");
+    float peso = scan.nextFloat();
 
+    System.out.println("Digite a aultura:");
+    float altura = scan.nextFloat();
+    
+    scan.close();
+    
+    float imc = peso / (altura * altura);
+
+    System.out.println(imc);
+
+  }
 }
